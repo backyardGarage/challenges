cc41d056877385203f86975c2ceaf68a9ce3c4e7
ybyyng <ybyyng@gmail.com>
2020-12-13 23:34:53 +0900

    Array training

---
+++ codewars/Go/8kyu/Beginner - Reduce but Grow/tries/main.go
@@ -0,0 +1,14 @@
+// Array training
+// source: https://ehpub.co.kr/go-%EC%96%B8%EC%96%B4-30-%EB%B0%B0%EC%97%B4%EC%9D%98-%EC%9A%94%EC%86%8C%EB%A5%BC-%EC%88%9C%EC%B0%A8%EC%A0%81%EC%9C%BC%EB%A1%9C-%EB%B0%A9%EB%AC%B8%ED%95%98%EA%B8%B0/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := [5]int{12, 34, 23, 56, 34}
+
+	for i, value := range arr {
+		fmt.Println(i, ":", value)
+	}
+}
